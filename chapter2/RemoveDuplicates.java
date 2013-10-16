140d0bf9860b3b4e8d6a0314ec42a770dd7877b9
zzMOM <wuwei1107@gmail.com>
2013-10-15 21:10:07 -0700

    RemoveDuplicates.java

--- chapter2/RemoveDuplicates.java
+++ chapter2/RemoveDuplicates.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 
 public class RemoveDuplicates {
 
+	//method use additional buffer, O(N)
 	public void deleteDups1(Node n){
 		Hashtable<Object, Boolean> table = new Hashtable<Object, Boolean>();
 		Node previous = null;
@@ -21,6 +22,33 @@ public class RemoveDuplicates {
 				System.out.print(n.elem.toString() + " ");
 			}
 			n = n.next;
+		}
+	}
+	
+	//method not use temporary buffer, O(1)space, O(n^2) time
+	public void deleteDups2(Node head){
+		if(head == null) return;
+		
+		Node current = head;
+		while(current != null){
+			/*remove all future nodes that have the same values*/
+			Node runner = current;
+			while(runner.next != null){
+				//toString().charAt(0) to compare, or it will be wrong
+				if(runner.next.elem.toString().charAt(0) == current.elem.toString().charAt(0)){
+					runner.next = runner.next.next;
+				} else {
+					runner = runner.next;
+				}
+			}
+			current = current.next;
+		}
+		
+		current = head;
+		while(current != null){
+
+			System.out.print(current.elem.toString() + " ");
+			current = current.next;
 		}
 	}
 }
