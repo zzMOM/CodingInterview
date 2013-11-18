64042e10962656481d21c9b874ae2962a63d9155
zzMOM <wuwei1107@gmail.com>
2013-11-18 10:28:10 -0800

    update callHandler

---
+++ callcenter/CallHandler.java
@@ -0,0 +1,10 @@
+package callcenter;
+
+public class CallHandler {
+	/*Initialize 10 respondents, 4 manager, 2 director*/
+	private final int NUM_RESPONDENTS = 10;
+	private final int NUM_MANAGER = 4;
+	private final int NUM_DIRECTOR = 2;
+	
+	
+}
