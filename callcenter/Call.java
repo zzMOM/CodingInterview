774956a0fe7b3e507175c5f9b1f01285be3f5421
zzMOM <wuwei1107@gmail.com>
2013-11-18 22:57:07 -0800

    update call

--- callcenter/Call.java
+++ callcenter/Call.java
@@ -20,6 +20,14 @@ public class Call {
 		handler = e;
 	}
 	
+	public Employee getHandler(){
+		return handler;
+	}
+	
+	public Caller getCaller(){
+		return caller;
+	}
+	
 	//employee responsor get the caller name first 
 	public void setCallerName(String name){
 		caller.setCallerName(name);
@@ -29,4 +37,13 @@ public class Call {
 	public void disconnect(){}
 	public Rank getRank(){ return null; }
 	public void setRank(Rank r){ rank = r; }
+	public void increaseRank(){
+		if(rank == Rank.Respondent){
+			rank = Rank.Manager;
+		} else if(rank == Rank.Manager){
+			rank = Rank.Director;
+		} else {
+			rank = null;
+		}
+	}
 }
