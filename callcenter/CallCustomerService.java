280e6860be9f89b822cec0b94fe42f1e46bdd2b9
zzMOM <wuwei1107@gmail.com>
2013-11-19 10:00:50 -0800

    update callcustomerservice

--- callcenter/CallCustomerService.java
+++ callcenter/CallCustomerService.java
@@ -1,17 +1,28 @@
 package callcenter;
 
-public class CallCustomerService extends Thread{
+import java.util.Random;
+
+public class CallCustomerService implements Runnable{
 	CallHandler callHandler;
+	Thread t;
+	
 	CallCustomerService(CallHandler callHandler){
 		this.callHandler = callHandler;
+		t = new Thread(this);
+		t.start();
 	}
-	
-	public void Run(){
-		//assume 20 calls coming in
-		for(int i = 0; i < 20; i++){
-			String phoneNumber = Integer.toString(1000000000 + (i + 12)*1234567);
-			Caller caller = new Caller(phoneNumber);
-			callHandler.dispatchCall(caller);
-		}
+
+
+	@Override
+	public void run() {
+		Random random = new Random();
+		int x = random.nextInt(50);
+		String phoneNumber = Integer.toString(1000000000 + x * 1234567);
+		System.out.println(phoneNumber + " is calling!");
+		Caller caller = new Caller(phoneNumber);
+		callHandler.dispatchCall(caller);
+		
 	}
+
+
 }
